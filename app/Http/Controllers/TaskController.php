efb129e297820027cbf83d45dc6c862161ceeec2
tsubasa kondo <ec2-user@ip-172-31-94-246.ec2.internal>
2019-05-30 08:44:45 +0000

    auth check

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,154 +1,117 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
+use App\Http\Controllers\Controller;
 use App\Task;
-
 class TaskController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    // getでtasks/にアクセスされた場合の「一覧表示処理」
     public function index()
     {
-         $data = [];
+        $data = [];
         if (\Auth::check()) {
             $user = \Auth::user();
             $tasks = $user->tasks()->orderBy('created_at', 'desc')->paginate(10);
-            
             $data = [
-                
+                'user' => $user,
                 'tasks' => $tasks,
             ];
-            return view('tasks.index',[
-            'tasks' => $tasks,
-            ]);
+            //$data += $this->counts($user);
+            return view('tasks.index', $data);
+        }else {
+            return view('welcome');
         }
-        
-        return view('welcome', $data);
-    
-        $tasks = Task::all();
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    // getでtasks/createにアクセスされた場合の「新規登録画面表示処理」
     public function create()
     {
-         if (\Auth::check()) {
-            $user = \Auth::user();
-        $task = new Task;
-        }
-        
-        return view('tasks.create',[
-            'task' => $task,
+        if (\Auth::check()) {
+            $task = new Task;
+    
+            return view('tasks.create', [
+                'task' => $task,
             ]);
+        }else {
+            return view('welcome');
+        }
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    // postでtasks/にアクセスされた場合の「新規登録処理」
     public function store(Request $request)
     {
-        $this->validate($request, [
-        'content' => 'required|max:191',
-        'status' => 'required|max:10',
-        ]);
-        
         if (\Auth::check()) {
-            $user = \Auth::user();
-            
-        $task = new Task;
-        $task->status = $request->status;
-        $task->content = $request->content;
-        $task->user_id = $user->id;
-        $task->save();
-        }
-        
-        return redirect('/');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-     
-    public function show($id){
-        
-         if (\Auth::check()) {
-            $user = \Auth::user();
-        $task = Task::find($id);
-        }
-        
-        return view('tasks.show',[
-            'task' => $task,
+            $this->validate($request, [
+                'status' => 'required|max:10',
+                'content' => 'required|max:191',
             ]);
+    
+            $request->user()->tasks()->create([
+                'status' => $request->status,
+                'content' => $request->content,
+            ]);
+    
+            return redirect('/');
+        }else {
+            return view('welcome');
+        }
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    // getでtasks/idにアクセスされた場合の「取得表示処理」
+    public function show($id)
+    {
+        $task = Task::find($id);
+        if (\Auth::id() === $task->user_id) {
+    
+            return view('tasks.show', [
+                'task' => $task,
+            ]);
+        }else {
+            return redirect('/');
+        }
+    }
+    // getでtasks/id/editにアクセスされた場合の「更新画面表示処理」
     public function edit($id)
     {
         $task = Task::find($id);
-        
-        return view('tasks.edit',[
-            'task' => $task,
+        if (\Auth::id() === $task->user_id) {
+    
+            return view('tasks.edit', [
+                'task' => $task,
             ]);
+        }else {
+            return redirect('/');
+        }
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    // putまたはpatchでtasks/idにアクセスされた場合の「更新処理」
     public function update(Request $request, $id)
     {
-         if (\Auth::check()) {
-            $user = \Auth::user();
-        $this->validate($request, [
-        'content' => 'required|max:191',
-        'status' => 'required|max:10',
-        ]);
-        
-        $task = Task::find($id);
-        $task->status = $request->status;
-        $task->content = $request->content;
-        $task->save();
+        if (\Auth::check()) {
+            $this->validate($request, [
+                'status' => 'required|max:10',
+                'content' => 'required|max:191',
+            ]);
+    
+            $task = Task::find($id);
+            $task->status = $request->status;
+            $task->content = $request->content;
+            $task->save();
+    
+            return redirect('/');
+        }else {
+            return view('welcome');
         }
-        return redirect('/');
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    // deleteでtasks/idにアクセスされた場合の「削除処理」
     public function destroy($id)
     {
-        $task = \App\Task::find($id);
-        
-        if (\Auth::id() === $task->user_id){
-            $task->delete();
+        if (\Auth::check()) {
+            $task = Task::find($id);
+    
+             if (\Auth::id() === $task->user_id) {
+                $task->delete();
+             }
+    
+            return redirect('/');
+        }else {
+            return view('welcome');
         }
-        return back();
     }
 }
-
